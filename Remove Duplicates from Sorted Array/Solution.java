1563628bb05f5ec46121d21b432543455a44a68b
Rasika Deodhar <rasikadeodhar95@gmail.com>
2021-06-30 23:39:56 +0100

    fresh look - remove duplicates from sorted array -> optimized solution

--- Remove Duplicates from Sorted Array/Solution.java
+++ Remove Duplicates from Sorted Array/Solution.java
@@ -1,63 +1,44 @@
 import java.util.Scanner;
 
 class Solution {
-    public int removeDuplicates(int[] nums) {
+    public static int removeDuplicates(int[] nums) {
 
-        //Very long solution - not much optimized
-        int duplicate = 0, flag = 0, i = 0, j = 0;
-
-        if (nums.length == 0)
+        if (nums == null || nums.length == 0) {
             return 0;
-        if (nums.length == 1)
-            return 1;
-
-        while (i < nums.length) {
-            if (j < nums.length - 1 && nums[j] != -100000 && nums[j] != nums[j + 1]) {
-                duplicate++;
-            }
-            if (j == nums.length-1 && nums[j] != -100000 && nums[j] != nums[j - 1]) {
-                duplicate++;
-            }
-            while (j < nums.length - 1 && nums[j] != -100000 && nums[j] == nums[j + 1]) {
-                flag++;
-                j++;
-            }
-            if (flag > 0 && j <= nums.length - 1)
-                duplicate++;
-            while (flag > 0) {
-                for (int k = i; k < nums.length - 1; k++) {
-                    nums[k] = nums[k + 1];
-                    if (k == nums.length - 2) {
-                        nums[k + 1] = -100000;
-                    }
-                }
-                flag--;
-            }
-            i++;
-            j = i;
-            flag = 0;
         }
 
-        return duplicate;
+        int count = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                count++;
+            }
+        }
+
+        int i = 0, j = 0;
+        while (i++ < count) {
+            while (j < nums.length - 1 && nums[j] == nums[j + 1]) {
+                j++;
+            }
+            nums[i] = nums[j++ + 1];
+        }
+
+        System.out.println("--Array---");
+        for (int k : nums) {
+            System.out.println(k);
+        }
+        System.out.println("---Solution--");
+
+        return count + 1;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int m = sc.nextInt();
-        int[] arr = new int[m];
-        int i = 0;
-        while (m-- > 0) {
-            arr[i++] = sc.nextInt();
-        }
-
-        Solution s = new Solution();
-        System.out.println(s.removeDuplicates(arr));
-
-        for (int j = 0; j < arr.length; j++) {
-            System.out.print(arr[j] + " ");
-        }
-
-        sc.close();
+        System.out.println(Solution.removeDuplicates(new int[] { 1, 2, 3, 4, 5 }));
     }
+
+    // Time Complexity -
+    // Best Case = No repeated elements -> O(1) -> only calculate the count
+    // Avg Case = Few elements repeated -> O(N)
+    // Worst Case = Many elements repeated -> O(N)
+
+    // Space Complexity -> O(N)
 }
