e7fcf74b2867250cb1807842b02761881081dcff
Rasika Deodhar <rasikadeodhar95@gmail.com>
2021-07-07 23:57:59 +0100

    Fresh look

--- Add Binary/Solution.java
+++ Add Binary/Solution.java
@@ -1,77 +1,46 @@
 class Solution {
 
-    public String addition(String a, String b) {
-        String result = "";
-        char carry = '0';
+    public static String addition(String a, String b) {
 
-        for (int i = a.length() - 1; i >= 0; i--) {
-            if ((int) (char) (a.charAt(i) ^ b.charAt(i)) == 0 && carry == '0') {
-                if (a.charAt(i) == '1' || b.charAt(i) == '1') {
-                    carry = '1';
-                }
-                result += '0';
-            } else if ((int) (char) (a.charAt(i) ^ b.charAt(i)) == 1 && carry == '0'
-                    || (int) (char) (a.charAt(i) ^ b.charAt(i)) == 0 && carry == '1') {
-                if (a.charAt(i) == '1' && b.charAt(i) == '1') {
-                    result += '1';
-                    carry = '1';
-                } else {
-                    result += '1';
-                    carry = '0';
-                }
-            } else if ((int) (char) (a.charAt(i) ^ b.charAt(i)) == 1 && carry == '1') {
-                if (a.charAt(i) == '1' && b.charAt(i) == '1') {
-                    result += '1';
-                    carry = '1';
-                } else {
-                    result += '0';
-                    carry = '1';
-                }
-                // result += '0';
-                // carry = '1';
+        if (a == null || b == null || a.length() == 0 || b.length() == 0) {
+            return null;
+        }
+
+        Double da = 0D, db = 0D, dr = 0D;
+        String res = "";
+
+        for (int i = a.length() - 1, j = 0; i >= 0; i--, j++) {
+            da += Math.pow(2, j) * Double.valueOf(a.substring(i, i + 1));
+        }
+        for (int i = b.length() - 1, j = 0; i >= 0; i--, j++) {
+            db += Math.pow(2, j) * Double.valueOf(b.substring(i, i + 1));
+        }
+
+        dr = da + db;
+
+        if (dr == 0) {
+            return "0";
+        }
+
+        for (; dr > 0D; dr /= 2D) {
+            res += String.valueOf((int) (dr % 2D));
+        }
+
+        StringBuilder final_res = new StringBuilder(res).reverse();
+        if (res.substring(0, 1) == "0") {
+            int i = 0;
+            for (i = 0; i < res.length() && res.substring(i, i + 1).equals("0"); i++) {
             }
-        }
-        if (carry == '1') {
-            result += '1';
-        }
 
-        result = new StringBuilder(result).reverse().toString();
-        return result;
-    }
-
-    public String addBinary(String a, String b) {
-
-        if (a.length() == 0) {
-            return b;
-        }
-        if (b.length() == 0) {
-            return a;
-        }
-        int length_diff = Math.abs(a.length() - b.length());
-
-        if (a.length() > b.length()) {
-            String add_on = "";
-            for (int i = 0; i < length_diff; i++) {
-                add_on += "0";
-            }
-            String new_b = add_on + b;
-            return addition(a, new_b);
-        } else if (a.length() < b.length()) {
-            String add_on = "";
-            for (int i = 0; i < length_diff; i++) {
-                add_on += "0";
-            }
-            String new_a = add_on + a;
-            return addition(new_a, b);
+            // final_res = res(i,)
         }
 
-        return addition(a, b);
-
+        return new StringBuilder(res).reverse().toString();
     }
 
     public static void main(String args[]) {
-        Solution s = new Solution();
-        String a = "1111", b = "1111";
-        System.out.println(s.addBinary(a, b));
+        System.out.println(Solution.addition(
+                "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+                "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"));
     }
 }
