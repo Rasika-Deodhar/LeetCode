2eb4ff0d5a917d66ce763ec096a3d98d70af377f
Rasika Deodhar <rasikadeodhar95@gmail.com>
2021-08-04 00:00:17 +0100

    Tried resolving test case - unsuccessful

--- Design HashMap/MyHashMap.java
+++ Design HashMap/MyHashMap.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class MyHashMap {
 
     int[][] map;
@@ -65,14 +69,63 @@ class MyHashMap {
 
     public static void main(String[] args) {
         MyHashMap mhm = new MyHashMap();
-        mhm.put(1, 1);
-        mhm.put(2, 2);
-        System.out.println(mhm.get(1));
-        System.out.println(mhm.get(3));
-        mhm.put(2, 1);
-        System.out.println(mhm.get(2));
-        mhm.remove(2);
-        System.out.println(mhm.get(2));
+        // mhm.put(1, 1);
+        // mhm.put(2, 2);
+        // System.out.println(mhm.get(1));
+        // System.out.println(mhm.get(3));
+        // mhm.put(2, 1);
+        // System.out.println(mhm.get(2));
+        // mhm.remove(2);
+        // System.out.println(mhm.get(2));
+
+        List<String> ops = new ArrayList<>();
+        ops.addAll(Arrays.asList("MyHashMap", "remove", "put", "remove", "remove", "get", "remove", "put", "get",
+                "remove", "put", "put", "put", "put", "put", "put", "put", "put", "put", "put", "put", "remove", "put",
+                "put", "get", "put", "get", "put", "put", "get", "put", "remove", "remove", "put", "put", "get",
+                "remove", "put", "put", "put", "get", "put", "put", "remove", "put", "remove", "remove", "remove",
+                "put", "remove", "get", "put", "put", "put", "put", "remove", "put", "get", "put", "put", "get", "put",
+                "remove", "get", "get", "remove", "put", "put", "put", "put", "put", "put", "get", "get", "remove",
+                "put", "put", "put", "put", "get", "remove", "put", "put", "put", "put", "put", "put", "put", "put",
+                "put", "put", "remove", "remove", "get", "remove", "put", "put", "remove", "get", "put", "put"));
+        List<List<Integer>> numList = new ArrayList<>();
+        numList.addAll(Arrays.asList(Arrays.asList(), Arrays.asList(27), Arrays.asList(65, 65), Arrays.asList(19),
+                Arrays.asList(0), Arrays.asList(18), Arrays.asList(3), Arrays.asList(42, 0), Arrays.asList(19),
+                Arrays.asList(42), Arrays.asList(17, 90), Arrays.asList(31, 76), Arrays.asList(48, 71),
+                Arrays.asList(5, 50), Arrays.asList(7, 68), Arrays.asList(73, 74), Arrays.asList(85, 18),
+                Arrays.asList(74, 95), Arrays.asList(84, 82), Arrays.asList(59, 29), Arrays.asList(71, 71),
+                Arrays.asList(42), Arrays.asList(51, 40), Arrays.asList(33, 76), Arrays.asList(17),
+                Arrays.asList(89, 95), Arrays.asList(95), Arrays.asList(30, 31), Arrays.asList(37, 99),
+                Arrays.asList(51), Arrays.asList(95, 35), Arrays.asList(65), Arrays.asList(81), Arrays.asList(61, 46),
+                Arrays.asList(50, 33), Arrays.asList(59), Arrays.asList(5), Arrays.asList(75, 89),
+                Arrays.asList(80, 17), Arrays.asList(35, 94), Arrays.asList(80), Arrays.asList(19, 68),
+                Arrays.asList(13, 17), Arrays.asList(70), Arrays.asList(28, 35), Arrays.asList(99), Arrays.asList(37),
+                Arrays.asList(13), Arrays.asList(90, 83), Arrays.asList(41), Arrays.asList(50), Arrays.asList(29, 98),
+                Arrays.asList(54, 72), Arrays.asList(6, 8), Arrays.asList(51, 88), Arrays.asList(13),
+                Arrays.asList(8, 22), Arrays.asList(85), Arrays.asList(31, 22), Arrays.asList(60, 9), Arrays.asList(96),
+                Arrays.asList(6, 35), Arrays.asList(54), Arrays.asList(15), Arrays.asList(28), Arrays.asList(51),
+                Arrays.asList(80, 69), Arrays.asList(58, 92), Arrays.asList(13, 12), Arrays.asList(91, 56),
+                Arrays.asList(83, 52), Arrays.asList(8, 48), Arrays.asList(62), Arrays.asList(54), Arrays.asList(25),
+                Arrays.asList(36, 4), Arrays.asList(67, 68), Arrays.asList(83, 36), Arrays.asList(47, 58),
+                Arrays.asList(82), Arrays.asList(36), Arrays.asList(30, 85), Arrays.asList(33, 87),
+                Arrays.asList(42, 18), Arrays.asList(68, 83), Arrays.asList(50, 53), Arrays.asList(32, 78),
+                Arrays.asList(48, 90), Arrays.asList(97, 95), Arrays.asList(13, 8), Arrays.asList(15, 7),
+                Arrays.asList(5), Arrays.asList(42), Arrays.asList(20), Arrays.asList(65), Arrays.asList(57, 9),
+                Arrays.asList(2, 41), Arrays.asList(6), Arrays.asList(33), Arrays.asList(16, 44),
+                Arrays.asList(95, 30)));
+
+        for (int k = 0; k < ops.size(); k++) {
+            switch (ops.get(k)) {
+                case "put":
+                    mhm.put(numList.get(k).get(0), numList.get(k).get(1));
+                    break;
+                case "get":
+                    System.out.println(mhm.get(numList.get(k).get(0)));
+                    break;
+                case "remove":
+                    mhm.remove(mhm.get(numList.get(k).get(0)));
+                    break;
+            }
+        }
     }
 }
 
