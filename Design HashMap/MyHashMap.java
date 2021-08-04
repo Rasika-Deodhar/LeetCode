762e7cbcb8575d38639c8de045af45f7238942d2
Rasika Deodhar <rasikadeodhar95@gmail.com>
2021-08-04 23:43:57 +0100

    Leetcode discussion solution

--- Design HashMap/MyHashMap.java
+++ Design HashMap/MyHashMap.java
@@ -4,79 +4,102 @@ import java.util.List;
 
 class MyHashMap {
 
-    int[][] map;
-    public static int i = -1;
+    // int[][] map;
+    // public static int i = -1;
 
-    /** Initialize your data structure here. */
+    // /** Initialize your data structure here. */
+    // public MyHashMap() {
+    // map = new int[10000][2];
+    // }
+
+    // /** value will always be non-negative. */
+    // public void put(int key, int value) {
+
+    // if (i > 10000) {
+    // return;
+    // }
+
+    // if (get(key) != -1) {
+    // for (int[] is : map) {
+    // if (is[0] == key) {
+    // is[1] = value;
+    // }
+    // }
+    // return;
+    // }
+
+    // if (MyHashMap.i == -1) {
+    // MyHashMap.i = 0;
+    // }
+
+    // map[MyHashMap.i][0] = key;
+    // map[MyHashMap.i][1] = value;
+    // MyHashMap.i++;
+    // }
+
+    // /**
+    // * Returns the value to which the specified key is mapped, or -1 if this map
+    // * contains no mapping for the key
+    // */
+    // public int get(int key) {
+    // int j = 0;
+    // for (int[] is : map) {
+    // if (is[0] == key) {
+    // return is[1];
+    // }
+    // if (j++ == MyHashMap.i) {
+    // return -1;
+    // }
+    // }
+    // return -1;
+    // }
+
+    // /**
+    // * Removes the mapping of the specified value key if this map contains a
+    // mapping
+    // * for the key
+    // */
+    // public void remove(int key) {
+    // int j = 0;
+
+    // if (get(key) == -1 || MyHashMap.i == -1) {
+    // return;
+    // }
+
+    // for (int[] is : map) {
+    // if (is[0] == key) {
+    // break;
+    // }
+    // j++;
+    // }
+
+    // for (; j <= i && j < map.length - 1; j++) {
+    // map[j][0] = map[j + 1][0];
+    // map[j][1] = map[j + 1][1];
+    // }
+    // MyHashMap.i--;
+    // }
+
+    // Much better solution from Leetcode discussions -
+    // https://leetcode.com/problems/design-hashmap/discuss/227081/Java-Solutions
+
+    int[] map;
+
     public MyHashMap() {
-        map = new int[10000][2];
+        map = new int[1000001];
+        Arrays.fill(map, -1);
     }
 
-    /** value will always be non-negative. */
-    public void put(int key, int value) {
-
-        if (i > 10000) {
-            return;
-        }
-
-        if (get(key) != -1) {
-            for (int[] is : map) {
-                if (is[0] == key) {
-                    is[1] = value;
-                }
-            }
-            return;
-        }
-
-        if (MyHashMap.i == -1) {
-            MyHashMap.i = 0;
-        }
-
-        map[MyHashMap.i][0] = key;
-        map[MyHashMap.i][1] = value;
-        MyHashMap.i++;
-    }
-
-    /**
-     * Returns the value to which the specified key is mapped, or -1 if this map
-     * contains no mapping for the key
-     */
     public int get(int key) {
-        int j = 0;
-        for (int[] is : map) {
-            if (is[0] == key) {
-                return is[1];
-            }
-            if (j++ == MyHashMap.i) {
-                return -1;
-            }
-        }
-        return -1;
+        return map[key];
     }
 
-    /**
-     * Removes the mapping of the specified value key if this map contains a mapping
-     * for the key
-     */
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+
     public void remove(int key) {
-        int j = 0;
-
-        if (get(key) == -1 || MyHashMap.i == -1) {
-            return;
-        }
-
-        for (int[] is : map) {
-            if (is[0] == key) {
-                break;
-            }
-            j++;
-        }
-
-        for (; j <= i && j < map.length - 1; j++) {
-            map[j][0] = map[j + 1][0];
-            map[j][1] = map[j + 1][1];
-        }
-        MyHashMap.i--;
+        map[key] = -1;
     }
 
     public static void main(String[] args) {
