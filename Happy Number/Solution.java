d8754205fd09aa5712c5ad449086634ef2065429
Rasika Deodhar <rasikadeodhar95@gmail.com>
2021-08-09 23:01:32 +0100

    Failing for big number

---
+++ Happy Number/Solution.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isHappy(int n) {
+
+        if (n == 0 || n >= 2 && n <= 6 || n == 8 || n == 9) {
+            return false;
+        }
+        if (isPower(10, n) || n == 1 || n == 7) {
+            return true;
+        }
+
+        return isHappy(squaredSum_n(n));
+    }
+
+    private int squaredSum_n(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            sum += Math.pow(n % 10, 2);
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    private boolean isPower(int x, int y) {
+
+        if (x == 1)
+            return (y == 1);
+
+        int pow = 1;
+        while (pow < y) {
+            pow *= x;
+        }
+        return (pow == y);
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isHappy(1999999999));
+    }
+}
