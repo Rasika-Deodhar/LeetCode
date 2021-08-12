5e3d6c0cf5b1d4c544ad6d3accb2587119c3511e
Rasika Deodhar <rasikadeodhar95@gmail.com>
2021-08-12 22:35:31 +0100

    Isomorphic Strings -> okayish solution

--- Isomorphic Strings/Solution.java
+++ Isomorphic Strings/Solution.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Objects;
+import java.util.Map.Entry;
 
 class Solution {
     public boolean isIsomorphic(String s, String t) {
@@ -12,12 +14,18 @@ class Solution {
                         return false;
                     }
                 }
+                if (hm.containsValue(t.substring(i)) && !getKeyFromValue(hm, t.substring(i)).equals(s.substring(i))) {
+                    return false;
+                }
                 break;
             }
             if (hm.get(s.substring(i, i + 1)) != null) {
                 if (!hm.get(s.substring(i, i + 1)).equals(t.substring(i, i + 1))) {
                     return false;
                 }
+            } else if (hm.containsValue(t.substring(i, i + 1))
+                    && !getKeyFromValue(hm, t.substring(i, i + 1)).equals(s.substring(i, i + 1))) {
+                return false;
             } else {
                 hm.put(s.substring(i, i + 1), t.substring(i, i + 1));
             }
@@ -27,8 +35,21 @@ class Solution {
 
     }
 
+    private String getKeyFromValue(HashMap<String, String> hm, String substring) {
+        for (Entry<String, String> h : hm.entrySet()) {
+            if (Objects.equals(h.getValue(), substring)) {
+                return h.getKey();
+            }
+        }
+        return "";
+    }
+
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.isIsomorphic("foo", "bar"));
+        System.out
+                .println(s.isIsomorphic("qwertyuiop[]asdfghjkl;'\\zxcvbnm,./", "',.pyfgcrl/=aoeuidhtns-\\;qjkxbmwvz"));
     }
+
+    // Time Complexity = O(N)
+    // Space Complexity = O(N)
 }
