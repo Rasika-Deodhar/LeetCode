88da8c8034b2632ff6295a6785772b5dc4155012
Rasika Deodhar <rasikadeodhar95@gmail.com>
2022-02-28 23:32:34 +0000

    Trying to solve using sliding window approach

--- Longest Substring Without Repeating Characters/Solution.java
+++ Longest Substring Without Repeating Characters/Solution.java
@@ -5,23 +5,50 @@ class Solution {
 
     // Solution not suitable for all test-cases
     public int lengthOfLongestSubstring(String s) {
+
+        // Not suitable for all codes
+        // if (s.equals(" ") || s.length() == 1) {
+        // return 1;
+        // }
+
+        // int result = 0;
+        // Map<String, Integer> m = new HashMap<>();
+
+        // for (int i = 0; i < s.length(); i++) {
+        // if (!m.containsKey(s.substring(i, i + 1))) {
+        // m.put(s.substring(i, i + 1), 1);
+        // } else {
+        // result = m.size() > result ? m.size() : result;
+        // m.clear();
+        // --i;
+        // }
+        // }
+
+        // result = m.size() > result ? m.size() : result;
+
+        // return result;
+
+        // Sliding Window Problem
+        if (s.equals(" ") || s.length() == 1) {
+            return 1;
+        }
+
         int result = 0;
         Map<String, Integer> m = new HashMap<>();
 
-        for (int i = 0; i < s.length(); i++) {
-            if (!m.containsKey(s.substring(i, i + 1))) {
-                m.put(s.substring(i, i + 1), 1);
-                result++;
+        for (int i = 0, j = i + 1; i < s.length() && j < s.length();) {
+            if (!m.containsKey(s.substring(i, j)) && s.substring(i, j).contains(s.subSequence(i, i + 1))) {
+
             }
         }
 
-        return result;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.lengthOfLongestSubstring("abcabcbb"));
-        System.out.println(s.lengthOfLongestSubstring("bbbbb"));
-        System.out.println(s.lengthOfLongestSubstring("pwwkew"));
+        // System.out.println(s.lengthOfLongestSubstring("abcabcbb"));
+        // System.out.println(s.lengthOfLongestSubstring("bbbbb"));
+        // System.out.println(s.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(s.lengthOfLongestSubstring("dvdf"));
     }
 }
